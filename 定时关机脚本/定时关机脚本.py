7099710248e76a36af2021c197b762d2461728dd
CK627 <3340324320@qq.com>
2023-12-24 14:25:55 +0800

    Add files via upload

---
+++ 定时关机脚本/定时关机脚本.py
@@ -0,0 +1,33 @@
+# -*- coding = utf-8 -*-
+# @Time:2023/8/19 20:45
+# @Author:CK
+# @File:定时关机脚本
+# @Software:PyCharm
+import os
+import time
+input_time=input("请输入关机时间，格式如：小时:分钟：")
+if input_time=="off":
+    os.system("shutdown -a")
+h1=int(input_time[0:2])
+m1=int(input_time[3:5])
+
+print(h1,m1)
+mytime=time.strftime("%H:%M:%S")
+h2=int(mytime[0:2])
+m2=int(mytime[3:5])
+
+
+if h1 > 24:
+    h1=24
+    m2=0
+if m1>60:
+    m1=60
+if h1>h2:
+    h1=h1+24
+
+s1=(h1+(m1/60.0)-h2-(m2/60.0))
+if s1<=0:
+    print("ERROR")
+else:
+    print("距离关机还有%d秒"%s1)
+    os.system("shutdown -s -t %d"%s1)
